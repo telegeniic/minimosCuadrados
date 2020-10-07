e9889d41074d124ac1a75ac60a00a49a3ab965e0
Martin Guajardo <martinguajardo20@gmail.com>
2020-10-07 11:08:34 -0500

    Se agrego el calculo para el coeficiente de relacion

--- minCuadrados.py
+++ minCuadrados.py
@@ -1,6 +1,7 @@
 # Minimos Cuadrados
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from os import system, name
 
 # ---Variables a usar ---
@@ -11,6 +12,12 @@ y_axis = []  # Datos del eje y
 xTy = []  # Lista con el resultado de x * y
 x_square = []  # lista con el resultado de x^2
 y_axis_plot = []  # Lista con y despues del metodo de minimos cuadrados
+x_less_xprom = []  # Lista con x menos el promedio de x
+y_less_yprom = []  # lista con y menos el promedio de y
+x_less_xprom_sqrt = []  # Lista con x menos el promedio de x, todo al cuadrado
+y_less_yprom_sqrt = []  # Lista con y menos el promedio de y, todo al cuadrado
+# Lista con x menos el promedio de x multiplicado por y menos el promedio de y
+xlp_times_ylp = []
 table = []  # Lista de apoyo para hacer la tabla
 colLabels = ["x", "y", "x*y", "x^2", "y=mx+b"]
 
@@ -74,8 +81,28 @@ for value in x_axis:
     y_value = float("{:.2f}".format(y_value))
     y_axis_plot.append(y_value)
 
-print(m)
-print(b)
+
+promx = sum(x_axis) / n
+promy = sum(y_axis) / n
+
+for x in x_axis:
+    x_less_xprom.append(x-promx)
+
+for x in x_less_xprom:
+    x_less_xprom_sqrt.append(x*x)
+
+for y in y_axis:
+    y_less_yprom.append(y-promy)
+
+for y in y_less_yprom:
+    y_less_yprom_sqrt.append(y*y)
+
+for i in range(n):
+    xlp_times_ylp.append(x_less_xprom[i]*y_less_yprom[i])
+
+r = sum(xlp_times_ylp) / (math.sqrt(sum(x_less_xprom_sqrt))
+                          * math.sqrt(sum(y_less_yprom_sqrt)))
+
 
 # Tabla
 table.append(x_axis)
@@ -92,7 +119,7 @@ gra.plot(x_axis, y_axis, 'ks', x_axis, y_axis_plot)
 gra.set_title(tittle)
 gra.set(xlabel=xTittle, ylabel=yTittle)
 trans = gra.get_xaxis_transform()
-gra.annotate(f"m = {m}\nb = {b}", xy=(x_axis[-1], y_axis_plot[-1]),
+gra.annotate(f"m = {m}\nb = {b}\nr = {r}", xy=(x_axis[-1], y_axis_plot[-1]),
              xycoords='data', textcoords="offset points", xytext=(30, 0))
 
 gtab = tab.table(cellText=tTable, colLabels=colLabels,
